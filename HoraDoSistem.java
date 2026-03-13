c647d0257207db32441f3be7dd2c962b1106cef2
kevin krystin <kevinkrystin@gmail.com>
2026-03-13 05:56:21 -0300

    Add files via upload

---
+++ HoraDoSistem.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package horadosistem;
+
+import java.util.Date;
+
+
+
+
+/**
+ *
+ * @author user
+ */
+public class HoraDoSistem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Date relogio = new Date();
+        System.out.println("A hora do sistema é:");
+        System.out.println(relogio.toString());
+        
+    }
+    
+}
